1793e082b7dbd228103afac479a8860390c8dfb9
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-13 23:38:27 +0600

    clear

--- src/main.jsx
+++ src/main.jsx
@@ -1,49 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Roots from './Compontents/Roots.jsx';
-import Home from './Compontents/Home.jsx';
-import Login from './Compontents/Login.jsx';
-import Register from './Compontents/Register.jsx';
-import AuthProviders from './Providers/AuthProviders.jsx';
-import Orders from './Compontents/Orders.jsx';
-import PrivateRouts from './routes/PrivateRouts.jsx';
-
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Roots></Roots>,
-    children: [
-      {
-        path: '/',
-        element: <Home></Home>
-      },
-      {
-        path: '/login',
-        element: <Login></Login>
-      },
-      {
-        path: '/register',
-        element: <Register></Register>
-      },
-      {
-        path:'/orders',
-        element:<PrivateRouts><Orders></Orders></PrivateRouts>
-      }
-    ]
+    element: <div>tanzid</div>,
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProviders>
       <RouterProvider router={router} />
-    </AuthProviders>
   </StrictMode>,
 )
